e9cc8d9aed6aae513c87808f5c113d7415ced7d3
PC-ROWIN\rowin <rowin_ruizendaal@hotmail.com>
2021-04-06 14:21:05 +0200

    [setup: socketIO]

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const router = require('./routes/router');
+const io = require('socket.io')(http)
+
+const port = process.env.PORT || 3000;
+
+app.use(router);
+app.use(express.static(`${__dirname}/public`));
+
+app.set('view engine', 'ejs');
+app.set('views', 'view');
+
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+
+    socket.on('message', (message) => {
+        io.emit('message', message)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+})
+
+
+http.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
